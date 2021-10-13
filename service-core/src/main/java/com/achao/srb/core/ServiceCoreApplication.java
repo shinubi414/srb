f7bfdc60067a5bbe3e6c79462d3f9aebf9f43454
luochao <384338581@qq.com>
2021-10-14 01:15:17 +0800

    1

---
+++ service-core/src/main/java/com/achao/srb/core/ServiceCoreApplication.java
@@ -0,0 +1,14 @@
+package com.achao.srb.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.achao.srb","com.achao.common"})
+public class ServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCoreApplication.class, args);
+    }
+}
