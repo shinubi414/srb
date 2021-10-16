1c2fcd3178864bde69a8da76ddcbd6c6b263b50a
luochao <384338581@qq.com>
2021-10-16 23:07:27 +0800

    11

--- service-core/src/main/java/com/achao/srb/core/service/impl/UserLoginRecordServiceImpl.java
+++ service-core/src/main/java/com/achao/srb/core/service/impl/UserLoginRecordServiceImpl.java
@@ -9,6 +9,7 @@ import com.cloopen.rest.sdk.CCPRestSmsSDK;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Set;
 
 /**
@@ -24,8 +25,8 @@ public class UserLoginRecordServiceImpl extends ServiceImpl<UserLoginRecordMappe
 
 
     @Override
-    public void sendSMS() {
-        //生产环境请求地址：app.cloopen.com
+    public void sendSMS(String phoneNum) {
+
         String serverIp = "app.cloopen.com";
         //请求端口
         String serverPort = "8883";
@@ -39,13 +40,14 @@ public class UserLoginRecordServiceImpl extends ServiceImpl<UserLoginRecordMappe
         sdk.setAccount(accountSId, accountToken);
         sdk.setAppId(appId);
         sdk.setBodyType(BodyType.Type_JSON);
-        String to = "13246837157";
+        String to = phoneNum;
         String templateId= "1";
-        String[] datas = {"变量1","变量2","变量3"};
-        String subAppend="1234";  //可选 扩展码，四位数字 0~9999
-        String reqId="fadfafas";  //可选 第三方自定义消息id，最大支持32位英文数字，同账号下同一自然天内不允许重复
-        //HashMap<String, Object> result = sdk.sendTemplateSMS(to,templateId,datas);
-        HashMap<String, Object> result = sdk.sendTemplateSMS(to,templateId,datas,subAppend,reqId);
+        String code = String.valueOf(new Random().nextInt(9999));
+        String[] datas = {code,"10","2333"};
+//        String subAppend="1234";  //可选 扩展码，四位数字 0~9999
+//        String reqId="fadfafas";  //可选 第三方自定义消息id，最大支持32位英文数字，同账号下同一自然天内不允许重复
+        HashMap<String, Object> result = sdk.sendTemplateSMS(to,templateId,datas);
+        //HashMap<String, Object> result = sdk.sendTemplateSMS(to,templateId,datas,subAppend,reqId);
         if("000000".equals(result.get("statusCode"))){
             //正常返回输出data包体信息（map）
             HashMap<String,Object> data = (HashMap<String, Object>) result.get("data");
